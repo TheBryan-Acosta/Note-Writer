b35d8e9d1d2bc021201d964851f41c546bb2923a
Bryan A <bryanacostadelacruz@gmail.com>
2022-01-09 02:33:03 -0600

    finalized and commentated

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { filterByQuery } = require("./lib/notes");
+const {
+	filterByQuery,
+	createNewNote,
+	validateNote,
+	deleteNote,
+} = require("./lib/notes");
 
-console.log(notes);
 //middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,7 +23,31 @@ app.get("/api/notes", (req, res) => {
 	if (req.query) {
 		results = filterByQuery(req.query, results);
 	}
+	//respond with our filtered notes
 	res.json(results);
+});
+
+app.post("/api/notes", (req, res) => {
+	// set id based on what the next index of the array will be
+	req.body.id = notes.length.toString();
+
+	//checks if the note is valid and expected before creating
+	if (!validateNote(req.body)) {
+		res.status(400).send("The note is not properly formatted.");
+	} else {
+		const note = createNewNote(req.body, notes);
+
+		//respond with our created note
+		res.json(note);
+	}
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+	const { id } = req.params;
+	const newNotes = deleteNote(id, notes);
+
+	//respond with our notes after deletion of note with matching id
+	res.json(newNotes);
 });
 
 // html routes
